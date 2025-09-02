18f1768402e2a11fbc523337bd66233cce929aac
JalilAbdullayev <abdullacelil00@gmail.com>
2025-09-02 07:55:34 +0400

    feat: create project layout and homepage
    - add TailwindCSS and AlpineJS
    - add Alpine collapse plugin
    - add Astro icon integration
    - add icon packs
    - create Footer and Header components
    - create Banner component
    - create Sponsors, SponsorSlider, SponsorSlide components
    - remove Welcome component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import alpinejs from "@astrojs/alpinejs";
+import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [
+    alpinejs({ entrypoint: "./src/scripts/entrypoint.ts" }),
+    icon(),
+  ],
+});
